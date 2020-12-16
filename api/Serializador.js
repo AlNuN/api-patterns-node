602e32370a606ed73bea91f1844cb4a74d2fa018
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-12-16 16:28:20 -0300

    Add xml support too

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,3 +1,4 @@
+const jsontoxml = require('jsontoxml');
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado');
 
 class Serializador {
@@ -5,11 +6,23 @@ class Serializador {
     return JSON.stringify(dados);
   }
 
+  xml (dados) {
+    let tag = this.tagSingular
+    if(Array.isArray(dados)) {
+      tag = this.tagPlural;
+      dados = dados.map((dado) => ({ [this.tagSingular]: dado }));
+    }
+    return jsontoxml({ [tag]: dados });
+  }
+
   serializar (dados){
+    dados = this.filtrar(dados);
+
     if (this.contentType === 'application/json')
-      return this.json(
-        this.filtrar(dados)
-      );
+      return this.json(dados);
+
+    if (this.contentType === 'application/xml')
+      return this.xml(dados);
 
     throw new ValorNaoSuportado(this.contentType)
   }
@@ -42,6 +55,8 @@ class SerializadorFornecedor extends Serializador {
       'empresa',
       'categoria',
     ].concat(camposExtras || []);
+    this.tagSingular = 'fornecedor';
+    this.tagPlural = 'fornecedores';
   }
 }
 
@@ -53,6 +68,8 @@ class SerializadorErro extends Serializador {
       'id',
       'mensagem',
     ].concat(camposExtras || []);
+    this.tagSingular = 'erro';
+    this.tagPlural = 'erros';
   }
 }
 
@@ -60,5 +77,5 @@ module.exports = {
   Serializador,
   SerializadorFornecedor,
   SerializadorErro,
-  formatosAceitos: ['application/json']
+  formatosAceitos: [ 'application/json', 'application/xml' ]
 }
